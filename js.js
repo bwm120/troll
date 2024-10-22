5fb1b7ae7c3f5ac7eb877d1884177fd7ab068d5d
bwm120 <lebidepzai@gmail.com>
2024-10-22 21:51:13 +0700

    bile

---
+++ js.js
@@ -0,0 +1,6 @@
+document.getElementById("btnDelete").addEventListener("click", function() {
+    alert("NGU VCL"); 
+})
+;document.getElementById("btnCancel").addEventListener("click", function() {
+    alert("CHET ME MAY DI"); 
+});
